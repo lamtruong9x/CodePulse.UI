b58c2a95ecd7378a71683a8cfc5415f727ffa858
Lam Truong <damlamtruong@gmail.com>
2023-10-02 08:19:51 +0700

    Environment: add environment files

--- src/app/core/features/category/services/category.service.ts
+++ src/app/core/features/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-resquest.model';
 import { Observable } from 'rxjs';
 import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +12,15 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>('https://localhost:7179/api/Categories', model);
+    return this.http.post<void>(
+      `${environment.apiBaseUrl}/api/Categories`,
+      model
+    );
   }
 
   getAllCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>('https://localhost:7179/api/Categories');
+    return this.http.get<Category[]>(
+      `${environment.apiBaseUrl}/api/Categories`
+    );
   }
 }
