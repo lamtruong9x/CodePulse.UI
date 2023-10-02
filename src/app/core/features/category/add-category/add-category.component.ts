3a57a0a7f38e4cb79db3105cfae36737689802b8
Lam Truong <damlamtruong@gmail.com>
2023-10-02 08:15:11 +0700

    Category: add getAllCategories service

--- src/app/core/features/category/add-category/add-category.component.ts
+++ src/app/core/features/category/add-category/add-category.component.ts
@@ -1,29 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-resquest.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css'],
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
+  addCategorySubscription?: Subscription;
+
   constructor(private categoryService: CategoryService) {
     this.model = {
       name: '',
       urlHandle: '',
     };
   }
+
   async onFormSubmit() {
     try {
-      this.categoryService.addCategory(this.model).subscribe({
-        next: (response) => {
-          console.log('This was successfull!');
-        },
-      });
+      this.addCategorySubscription = this.categoryService
+        .addCategory(this.model)
+        .subscribe({
+          next: (response) => {
+            console.log('This was successfull!');
+          },
+        });
     } catch (err) {
       console.log(err);
     }
+  }
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 }
--- src/app/core/features/category/services/category.service.ts
+++ src/app/core/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-resquest.model';
 import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,9 @@ export class CategoryService {
 
   addCategory(model: AddCategoryRequest): Observable<void> {
     return this.http.post<void>('https://localhost:7179/api/Categories', model);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>('https://localhost:7179/api/Categories');
   }
 }
